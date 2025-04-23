eb168987de040131e56f4dfc587bd4eaed087734
cho-sohyun <shteam1107@gmail.com>
2025-04-24 01:15:00 +0900

    주석(수정 예정 내용)

--- src/pages/MovieDetail/MovieDetailPage.jsx
+++ src/pages/MovieDetail/MovieDetailPage.jsx
@@ -8,7 +8,7 @@ import { useSimilarMoviesQuery } from "../../hooks/useSimilarMovies";
 // 상세 정보
 // 리뷰 보여주기 -> 더보기, 접기 추가
 // 추천 영화 보여주기
-// 예고편 보여주기
+// 예고편 보여주기 -> 유튜브 말고 티저 영상 스트리밍 가능한지 확인 (라이브러리 ?) -> 배너, 상세페이지 상단 이미지 영역
 const MovieDetailpage = ({ movie, onClose }) => {
   const { data: detailMovie, isLoading } = useDetailMoviesQuery(movie.id);
   const { data: reviews, isLoading: isLoadingReview } = useMovieReviewsQuery(
